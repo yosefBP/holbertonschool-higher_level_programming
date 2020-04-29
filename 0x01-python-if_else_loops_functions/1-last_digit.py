7f231dd33987305f3d2c7f0f0d1d11bf264c07d3
juankarlos999 <juan_karlos_999@hotmail.com>
2020-04-28 19:40:12 -0500

    program that validates the last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_dig = number % 10; last_dig2 = number % -10
+last_dig = number % 10
+last_dig2 = number % -10
 if number <= 0:
     if last_dig2 != 0:
         print("Last digit of {:d} is {:d} and is\
