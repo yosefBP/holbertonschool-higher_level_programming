54b6142e856f10e9aa59182156d2c041ae005592
juankarlos999 <juan_karlos_999@hotmail.com>
2020-05-01 00:15:39 -0500

    files

--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    if sentence:
-        tupla = (len(sentence), sentence[0])
-        return tupla
-    else:
-        tupla = (0, None)
-        return tupla
--- 0x03-python-data_structures/9-max_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def max_integer(my_list=[]):
-    if my_list:
-        my_list.sort(reverse=True)
-        maxim = my_list[0]
-        return maxim
-    else:
-        return None
