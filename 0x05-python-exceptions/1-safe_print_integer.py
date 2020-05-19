4988d98047ec950512776c9f9a7dbb957a5347df
juankarlos999 <juan_karlos_999@hotmail.com>
2020-05-18 21:42:15 -0500

    function that prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if not value:
+    if not value and value != 0:
         return (False)
     try:
         print("{:d}".format(value), end="")
