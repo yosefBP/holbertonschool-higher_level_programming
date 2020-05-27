412ce4cb65adc8a10f7b6cd74fced08da5cbab84
juankarlos999 <juan_karlos_999@hotmail.com>
2020-05-27 14:37:31 -0500

    class Rectangle with mothodsand variables

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+"""Class Rectanglei"""
 
 
 class Rectangle:
-    """ """
+    """Class Rectangle """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-    """ """
+    """ Method """
     @property
     def width(self):
         return self.__width
 
-    """ """
+    """Method """
     @width.setter
     def width(self, val):
         if not isinstance(val, int):
@@ -22,12 +22,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = val
 
-    """ """
+    """Method """
     @property
     def height(self):
         return self.__height
 
-    """ """
+    """Method """
     @height.setter
     def height(self, val):
         if not isinstance(val, int):
