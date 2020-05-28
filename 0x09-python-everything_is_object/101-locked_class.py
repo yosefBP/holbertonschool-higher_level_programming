38029c6804a4dc56733de9cdccc05ddf45a2c005
juankarlos999 <juan_karlos_999@hotmail.com>
2020-05-28 15:43:48 -0500

    Use slots to prevent the dynamic creation of attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Use __slots__ to prevent the dynamic creation of attributes """
+
+
 class LockedClass:
     """ Class without __dict__ and locked attributes """
     __slots__ = "first_name"
