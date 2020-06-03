2eadf6c6b09c1f00a1b3daf11b2039bc5f779f7f
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-02 23:56:39 -0500

    Function validate si the object is an instance of class or inherited

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""function that validate if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class"""
+"""function that validate if the object is an instance of,
+or if the object is an instance of a class that inherited from,
+the specified class"""
 
 
 def is_kind_of_class(obj, a_class):
