0690252826329256d525a2b24723e62a7312d346
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-03 00:35:47 -0500

    Funtion that validate if an object is inherited directly or indirectly

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function that validate if the object is an instance of a class that
+inherited (directly or indirectly) from the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False"""
+    if type(obj) == a_class:
+        return False
+    if isinstance(obj, a_class):
+        return True
