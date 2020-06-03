1dd743af0cbeae5e5eaa2272b21ec230dcce49af
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-02 23:49:58 -0500

    function that validate if the object an instance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that validate if the object is exactly an instance
+of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an instance of the specified class
+    ; otherwise False"""
+    return type(obj) == a_class
