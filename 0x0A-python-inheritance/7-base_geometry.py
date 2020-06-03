f9ba537ee45c5bd32e00ab94b20b61d084f6348b
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-03 02:32:41 -0500

    class BaseGeometry and methods

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry contains two methods Integer validator
+        and other empty method that handles an exception"""
+
+    def area(self):
+        """Raises an Exception with a message"""
+        message = "area() is not implemented"
+        raise Exception(message)
+
+    def integer_validator(self, name, value):
+        """This method validates *value is not an integer:
+            raise a TypeError; is less or equal to 0:
+            raise a ValueError"""
+        message1 = name + ' must be an integer'
+        message2 = name + ' must be an integer'
+        if isinstance(value, int) is False:
+            raise TypeError(message1)
+        if value <= 0:
+            raise ValueError(message2)
