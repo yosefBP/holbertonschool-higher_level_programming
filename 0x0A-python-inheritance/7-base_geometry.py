50e6f47e27b62a5c896bec5a3dcd8d0faac26d53
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-03 03:29:01 -0500

    [UPDATE] Conditional integer_validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,20 +4,21 @@
 
 class BaseGeometry:
     """class BaseGeometry contains two methods Integer validator
-        and other empty method that handles an exception"""
+    and other empty method that handles an exception
+    """
 
     def area(self):
         """Raises an Exception with a message"""
-        message = "area() is not implemented"
+        message = 'area() is not implemented'
         raise Exception(message)
 
     def integer_validator(self, name, value):
-        """This method validates *value is not an integer:
-            raise a TypeError; is less or equal to 0:
-            raise a ValueError"""
+        """Public instance method that validates value
+        value is an integer, name is a string
+        """
         message1 = name + ' must be an integer'
         message2 = name + ' must be greater than 0'
-        if type(value) != int:
+        if isinstance(value, int) is False:
             raise TypeError(message1)
         if value <= 0:
             raise ValueError(message2)
