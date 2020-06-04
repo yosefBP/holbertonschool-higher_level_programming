459df9868f8c57e82e874dc0339c05523bbee4df
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-04 00:02:36 -0500

    advanced task

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Search and update"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ function that inserts a line of text to a file, after each line
+    containing a specific string"""
+
+    with open(filename, "r+") as myfile:
+        lines = myfile.readlines()
+        newline = []
+        for i in range(len(lines)):
+            newline.append(lines[i])
+            if search_string in lines[i]:
+                newline.append(new_string)
+
+        myfile.seek(0)
+        myfile.write("".join(newline))
