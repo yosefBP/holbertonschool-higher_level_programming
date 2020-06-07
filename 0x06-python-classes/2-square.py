7c0e8e2a3daf229c2718a02c4b02609d136855d1
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-07 16:29:16 -0500

    handle raise TypeError and ValueError

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """Private instance attribute: size"""
+    def __init__(self, size=0):
+        """Function constructor"""
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self. __size = size
