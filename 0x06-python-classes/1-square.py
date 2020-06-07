5ca9eccf50d7a5b998c9a0b49239db171ae48c31
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-07 16:11:57 -0500

    Private instance attribute: size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,6 @@
 
 class Square:
     """Private instance attribute: size"""
-    def __init__(self)
-    """Function constructor"""
-    self.__size = size
+    def __init__(self):
+        """Function constructor"""
+        __size = 0
