38f635ff40f7ee16be93d13339851da5da79d755
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-07 21:00:12 -0500

    [ADD]:setter and getter position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,7 +10,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        if position[0] < 0 or position[1] < 0:
+        if position[0] < 0 or position[1] < 0 or\
+                isinstance(position, tuple) is False:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
