5f7dcf21077a8e6fb8adb7e55d1d5069544ea0cd
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-07 22:58:57 -0500

    [ADD]:setter and getter position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -60,7 +60,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            if self.__position[1] > 0:
+            if self.position[1] > 0:
                 print()
             for row in range(self.__size):
                 print(" " * self.__position[0], character * self.__size)
