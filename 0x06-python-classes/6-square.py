e10f239eeedebab1707d6e813a9bc7ebd7d8124b
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-07 22:57:15 -0500

    [ADD]:setter and getter position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,17 +37,16 @@ class Square:
     @position.setter
     def position(self, value):
         """position must be a tuple of 2 positive integers"""
+        if isinstance(value, tuple) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif isinstance(value, tuple) is False:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif isinstance(value[0], int) is False or\
+        if isinstance(value[0], int) is False or\
                 isinstance(value[1], int) is False:
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Public instance method that returns the current square area"""
