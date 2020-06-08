4eb5fd625170b665286f462be2b9c358e8c509e8
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-07 21:28:24 -0500

    [ADD]:setter and getter position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,7 +11,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
