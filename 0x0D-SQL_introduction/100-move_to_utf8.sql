b5a544d80e11a77ef68a98fd828f17a248ec8d80
juankarlos999 <juan_karlos_999@hotmail.com>
2020-07-13 23:06:30 -0500

    script that converts database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- script that converts hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
