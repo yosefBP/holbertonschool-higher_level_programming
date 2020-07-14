6a4e6f55887793d0ee136cf2cfc23d2e56495371
juankarlos999 <juan_karlos_999@hotmail.com>
2020-07-14 15:37:34 -0500

    script that lists all privileges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- script that lists all privileges of the MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS;
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-SHOW GRANTS FOR 'user_0d_3'@'localhost';
+SHOW GRANTS;
