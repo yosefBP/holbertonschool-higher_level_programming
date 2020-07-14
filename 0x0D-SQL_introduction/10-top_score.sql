1dd00c686a8d1beb25c055a66bce9649e936767f
juankarlos999 <juan_karlos_999@hotmail.com>
2020-07-13 21:56:17 -0500

    script that lists all records of a table used ORDER BY DEC

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table
-SELECT name, score FROM second_table ORDER BY  score DESC, name;
+SELECT score, name FROM second_table ORDER BY  score DESC, name;
