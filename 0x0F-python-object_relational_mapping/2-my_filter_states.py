73b1e2632a9de8f69f3ef086f8b4a0f99f2c724c
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-10 19:22:30 -0500

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Filter states by user input"""
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password_ = argv[2]
+    db_name = argv[2]
+    name_searched = argv[3]
+
+    db_conex = MySQLdb.connect(host="localhost", port=3306, user=username,
+                               passwd=password_,
+                               db=db_name)
+    cur = db_conex.cursor()
+    query = """SELECT states.id, states.name FROM states WHERE name='{:s}'
+    COLLATE latin1_general_cs ORDER BY states.id ASC;""".format(name_searched)
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db_conex.close()
