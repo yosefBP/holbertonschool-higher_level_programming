7edb67401c9005f2aba0a7728e841c05c8d51f5f
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-21 01:59:03 -0500

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL POST parameters
-curl -s -d '{"email":"hr@holbertonschool.com", "subject":"I will always be here for PLD"}' -H "Content-Type: application/json" -X POST "$1"
+curl -s -d 'email:hr@holbertonschool.com&subject:I will always be here for PLD' -X POST "$1"
